09150fd7c0b5e4a7c50d50cfb6828644b8f07add
Sietsewer <sietsewer@gmail.com>
2014-06-19 03:21:40 +0200

    initial commit

---
+++ android/TouchScreenController/gen/com/example/touchscreencontroller/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.touchscreencontroller;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ android/TouchScreenController/src/com/example/touchscreencontroller/Vector3f_simple.java
@@ -0,0 +1,19 @@
+package com.example.touchscreencontroller;
+
+public class Vector3f_simple {
+	public float x;
+	public float y;
+	public float z;
+	
+	public Vector3f_simple(){
+		this.x = 0.0f;
+		this.y = 0.0f;
+		this.z = 0.0f;
+	}
+	
+	public Vector3f_simple(float x, float y, float z){
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+}
